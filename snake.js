9d395b25b5826cf9443af26bef017bf63f9bedad
chiwentan <chiwentan@live.com>
2025-03-13 15:25:42 +0800

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,120 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+let snake = [{ x: 200, y: 200 }];
+let dx = 10;
+let dy = 0;
+let foodX;
+let foodY;
+
+const main = () => {
+    if (didGameEnd()) return;
+
+    setTimeout(() => {
+        clearCanvas();
+        drawFood();
+        advanceSnake();
+        drawSnake();
+
+        main();
+    }, 100);
+};
+
+const clearCanvas = () => {
+    ctx.fillStyle = '#333';
+    ctx.strokestyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+};
+
+const drawSnake = () => {
+    snake.forEach(drawSnakePart);
+};
+
+const drawSnakePart = (snakePart) => {
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokestyle = 'darkgreen';
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+};
+
+const advanceSnake = () => {
+    const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+    snake.unshift(head);
+
+    const didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+    if (didEatFood) {
+        createFood();
+    } else {
+        snake.pop();
+    }
+};
+
+const didGameEnd = () => {
+    for (let i = 4; i < snake.length; i++) {
+        const didCollide = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
+        if (didCollide) return true;
+    }
+
+    const hitLeftWall = snake[0].x < 0;
+    const hitRightWall = snake[0].x > canvas.width - 10;
+    const hitToptWall = snake[0].y < 0;
+    const hitBottomWall = snake[0].y > canvas.height - 10;
+
+    return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+};
+
+const createFood = () => {
+    foodX = Math.floor(Math.random() * 40) * 10;
+    foodY = Math.floor(Math.random() * 40) * 10;
+
+    snake.forEach((part) => {
+        const hasEaten = part.x === foodX && part.y === foodY;
+        if (hasEaten) createFood();
+    });
+};
+
+const drawFood = () => {
+    ctx.fillStyle = 'red';
+    ctx.strokestyle = 'darkred';
+    ctx.fillRect(foodX, foodY, 10, 10);
+    ctx.strokeRect(foodX, foodY, 10, 10);
+};
+
+const changeDirection = (event) => {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    const keyPressed = event.keyCode;
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+
+    if (keyPressed === LEFT_KEY && !goingRight) {
+        dx = -10;
+        dy = 0;
+    }
+
+    if (keyPressed === UP_KEY && !goingDown) {
+        dx = 0;
+        dy = -10;
+    }
+
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+        dx = 10;
+        dy = 0;
+    }
+
+    if (keyPressed === DOWN_KEY && !goingUp) {
+        dx = 0;
+        dy = 10;
+    }
+};
+
+document.addEventListener('keydown', changeDirection);
+
+createFood();
+main();
